63b70022ce5a07c7cb152d427188ca76887bdf01
Husanboy <samadovxusan9013@gmail.com>
2024-04-01 13:47:17 +0500

    [FOUNDATION] Add domain entities

---
+++ src/Eco-Market.Backend/EcoMarket.Domain/Common/Entities/ICreationAuditableEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EcoMarket.Domain.Common.Entities;
+
+public interface ICreationAuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the ID of the user who created the entity.
+    /// </summary>
+    Guid CreatedByUserId { get; set; }
+}
